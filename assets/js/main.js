80b05a6a1524e0177fe292bedbb2f941f472b531
David <davseveloff@gmail.com>
2021-01-21 16:22:39 +0200

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,57 @@
 
-//hide top nav on scroll down and show it again on scroll up
-let scrollPos = 0;
-const nav = document.getElementsByTagName("header")[0];
+function setUpEvents(){
+  let scrollPos = 0;
+  const header = document.getElementsByTagName("header")[0];
 
-function checkPosition() {
-  let windowY = window.scrollY;
-  if (windowY < scrollPos) {
-    // Scrolling UP
-    nav.classList.remove('header-up');
-  } else {
-    // Scrolling DOWN
-    nav.classList.add('header-up');
+  function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY < scrollPos) {
+      // Scrolling UP
+      header.classList.remove('header-up');
+    } else {
+      // Scrolling DOWN
+      header.classList.add('header-up');
+    }
+    scrollPos = windowY;
   }
-  scrollPos = windowY;
+
+  window.addEventListener('scroll', checkPosition);
+
+
+
+
+  const nav = document.querySelector(".topnav__menu"),
+    navToggle = document.querySelector(".topnav__bars");
+    if (navToggle) {
+      navToggle.addEventListener("click",
+      function(e) {
+      console.log('dav');
+      if (nav.classList.contains('open')){
+        nav.classList.remove('open');
+      } else {
+        nav.classList.add('open');
+      }
+      e.preventDefault();
+      }, false);
+    }
+
+    /* Any click outside of the nav element will close the menu if it's open */
+    var specifiedElement = document.querySelector('.topnav__menu');
+    document.addEventListener('click', function(event) {
+      var isClickInside = specifiedElement.contains(event.target);
+      if (!isClickInside && nav.className == "open") {
+        nav.className = "";
+      } 
+    });
+
 }
 
-window.addEventListener('scroll', checkPosition);
+
+
+
+//hide top nav on scroll down and show it again on scroll up
+
+
+window.onload = function(){
+  setUpEvents();
+}
